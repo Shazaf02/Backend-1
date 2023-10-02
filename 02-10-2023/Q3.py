f766dadb0a5eb04ab0ebd4d7db4327b701201aa6
Shazaf02 <125971676+Shazaf02@users.noreply.github.com>
2023-10-02 07:11:45 -0400

    Update Q3.py

--- 02-10-2023/Q3.py
+++ 02-10-2023/Q3.py
@@ -1,4 +1,8 @@
-def calculate_tax(bill,tax_rate):
-    return(bill*tax_rate)/100.00
-print('Total Tax:',calculate_tax(175.00,15))
-print('Total Tax:',calculate_tax(164.33,22))    
+def calculate_tax(bill, tax_rate):
+    tax_amount = (bill * tax_rate) / 100.00
+    print('Total Tax:', tax_amount)
+    return tax_amount
+
+calculate_tax(175.00, 15)
+calculate_tax(164.33, 22)
+
